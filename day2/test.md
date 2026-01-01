4cff0df4691d5eba4ecba9b24336146cba2359ed
Daniel paul <booyahkaasah@gmail.com>
2026-01-01 23:50:49 +0530

    day 2 part 2 done

---
+++ day2/test.md
@@ -0,0 +1,13 @@
+length x width
+width x height
+height x length
+
+4x23x21
+
+92
+483
+84
+
+2x92 + 2x483 + 2x84 + 84
+
+1,318
--- day2/test.ts
+++ day2/test.ts
@@ -1,50 +1,58 @@
 import * as fs from "fs";
+import { totalmem } from "os";
 import { constrainedMemory } from "process";
 
-fs.readFile(
-  "data.txt",
-  "utf8",
-  (err: NodeJS.ErrnoException | null, data: string) => {
-    if (err) throw err;
+fs.readFile("data.txt", "utf8", (err: NodeJS.ErrnoException | null, data: string) => {
+  if (err) throw err;
 
-    let lengthOrder: number = 0;
-    let widthOrder: number = 1;
-    let heightOrder: number = 2;
-    let sum: number = 0;
-    let rawArr: Array<string> = data.split("\n");
+  let lengthOrder: number = 0;
+  let widthOrder: number = 1;
+  let heightOrder: number = 2;
+  let sum: number = 0;
+  let ribbonSum: number = 0;
+  let rawArr: Array<string> = data.split("\n");
 
-    for (let giftSpec of rawArr) {
-      if (giftSpec == "" || giftSpec === null) continue;
+  for (let giftSpec of rawArr) {
+    if (giftSpec == "" || giftSpec === null) continue;
 
-      let min: number = Number.MAX_VALUE;
-      let area: number = 0;
-      let singleGiftPaper: number = 0;
-      let allDimensionValues: Array<string> = giftSpec.split("x");
-      let subTotalArea: number = 0;
-      let length: number = Number(allDimensionValues[lengthOrder]);
-      let width: number = Number(allDimensionValues[widthOrder]);
-      let height: number = Number(allDimensionValues[heightOrder]);
-      let side: number;
+    let min: number = Number.MAX_VALUE;
+    let area: number = 0;
+    let singleGiftPaper: number = 0;
+    let allDimensionValues: Array<string> = giftSpec.split("x");
+    let subTotalArea: number = 0;
+    let length: number = Number(allDimensionValues[lengthOrder]);
+    let width: number = Number(allDimensionValues[widthOrder]);
+    let height: number = Number(allDimensionValues[heightOrder]);
+    let side: number;
+    let ribbon: number;
+    let ribbonForBow: number;
+    let totalBowlength: number;
+    let smallestDimension: number;
+    let smallerDimension: number;
 
-      side = length * width;
-      subTotalArea = 2 * side;
-      area += subTotalArea;
-      if (min > side) min = side;
+    [smallestDimension, smallerDimension] = [length, width, height].sort((a, b) => a - b);
+    side = length * width;
+    subTotalArea = 2 * side;
+    area += subTotalArea;
+    if (min > side) min = side;
 
-      side = width * height;
-      subTotalArea = 2 * side;
-      area += subTotalArea;
-      if (min > side) min = side;
+    side = width * height;
+    subTotalArea = 2 * side;
+    area += subTotalArea;
 
-      side = height * length;
-      subTotalArea = 2 * side;
-      area += subTotalArea;
-      if (min > side) min = side;
+    side = height * length;
+    subTotalArea = 2 * side;
+    area += subTotalArea;
+    if (min > side) min = side;
 
-      singleGiftPaper = area + min;
+    singleGiftPaper = area + min;
+    ribbon = 2 * (smallestDimension + smallerDimension);
+    ribbonForBow = length * width * height;
+    totalBowlength = ribbonForBow + ribbon;
 
-      sum += singleGiftPaper;
-    }
-    console.log(sum);
-  },
-);
+    sum += singleGiftPaper;
+    ribbonSum += totalBowlength;
+  }
+  console.log(`Total wrapping paper needed is : ${sum}`);
+  console.log(`Total ribbon needed for total gifts: ${ribbonSum}`);
+});
