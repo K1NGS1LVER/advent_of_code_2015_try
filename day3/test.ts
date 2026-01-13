9951fb49771bdf1f798a34fb4833793ab9be035e
Daniel paul <booyahkaasah@gmail.com>
2026-01-14 00:23:53 +0530

    day 3 part one done with great efforts

--- day3/test.ts
+++ day3/test.ts
@@ -1,8 +1,45 @@
-import * as fs from "fs";
+import fs from "fs";
 
 fs.readFile("data.txt", "utf8", (err: NodeJS.ErrnoException | null, data: string) => {
-  let ddata: string = data;
-  for (let letter in ddata) {
-    console.log(`${letter} \n`);
+  if (err) throw err;
+
+  type cords = [number, number];
+
+  const convertNumberToStr = (ele: cords) => {
+    let [a, b] = ele;
+    return `${a},${b}`;
+  };
+
+  const convertStrToNumber = (ele: string): [number, number] => {
+    return ele.split(",").map(Number) as [number, number];
+  };
+
+  let raw: string = data.trim();
+  const mapped = new Set<string>();
+
+  let currentElement: cords = [0, 0];
+
+  mapped.add(convertNumberToStr(currentElement));
+  for (const direction of raw) {
+    switch (direction) {
+      case "<":
+        currentElement = [currentElement[0] - 1, currentElement[1]];
+        break;
+      case ">":
+        currentElement = [currentElement[0] + 1, currentElement[1]];
+        break;
+      case "v":
+        currentElement = [currentElement[0], currentElement[1] - 1];
+        break;
+      case "^":
+        currentElement = [currentElement[0], currentElement[1] + 1];
+        break;
+
+      default:
+        console.log("invalid characters");
+        break;
+    }
+    mapped.add(convertNumberToStr(currentElement));
   }
+  console.log(mapped.size);
 });
